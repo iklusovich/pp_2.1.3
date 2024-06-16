2f32c8173038a5d395a201c696a084ab65b33e03
i.klusovich <ilya.klusovich.89@mail.ru>
2024-06-16 21:38:52 +0300

    done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,20 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld helloWorld =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld helloWorld1 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(helloWorld == helloWorld1);
+        System.out.println(cat == cat1);
     }
 }
